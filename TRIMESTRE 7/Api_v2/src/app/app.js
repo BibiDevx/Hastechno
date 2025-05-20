562725ea105cc3667d31e1dc62fd09b7ca54cc86
Brian Beltran <babeltranp@gmail.com>
2025-05-20 11:44:45 -0500

    add api en node

---
+++ TRIMESTRE 7/Api_v2/src/app/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import routes from "../routers/user.router.js";
+import morgan from "morgan";
+
+const app = express();
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use('/api/v1',userRouter);
+
+app.use((rep, res, next) => {
+    res.status(404).json({error: "Endpoint no encontrado"});
+});
+export default app;
